e7975112314ac7d715ad0bae61c073510e1da9be
deepxshine <aevdok2800@gmail.com>
2023-11-24 18:32:06 +0300

    24.11

--- azimovblog/blog/admin.py
+++ azimovblog/blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post, Comments, Likes, Category
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('label', 'slug', 'get_post_count')
+    search_fields = ('label', 'slug')
+
+    def get_post_count(self, obj):
+        return obj.post_category.count()
+
+    get_post_count.short_description = 'Количество постов'
+
+
+@admin.register(Likes)
+class LikesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    pass
--- azimovblog/blog/models.py
+++ azimovblog/blog/models.py
@@ -6,23 +6,59 @@ from django.db import models
 User = get_user_model()
 
 
+class Category(models.Model):
+    label = models.CharField(max_length=255, verbose_name='Название')
+    description = models.CharField(max_length=2048, verbose_name="Описание")
+    slug = models.SlugField(unique=True, verbose_name='Уникальное имя')
+
+    class Meta:
+        ordering = ['label']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.label
+
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='post_author')
     text = models.TextField()
     image = models.ImageField(upload_to='posts/', blank=True)
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name='post_category')
     pubdate = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, through='Likes',
+                                   related_name='post_likes')
+    comments = models.ManyToManyField(User, through='Comments',
+                                      related_name='post_comments')
 
     class Meta:
         ordering = ['-pubdate']
         verbose_name = 'Пост'
-        verbose_name_plural = 'Посты' # verbose_name+'s'
+        verbose_name_plural = 'Посты'
 
     def __str__(self):
         return self.title
 
 
 class Likes(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='liked_post')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Лайки'
+        verbose_name_plural = 'Лайки'
+
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name='com_post')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=1024)
+
+    class Meta:
+        verbose_name = 'Комментарии'
+        verbose_name_plural = 'Комментарии'
